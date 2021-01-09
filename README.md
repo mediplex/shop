1a3ac704c64e15c24f0a5868636aa4def1a86ab7
Mehdi Karim <contact@mehdikarim.com>
2021-01-09 10:39:57 +0000

    add-redux

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usefull cmd:
+
+## Kill any process running on port 3000
+``` bash
+ kill -9 $(lsof -t -i:3000)   
+```
+## 
---
+++ src/components/Examples.tsx
@@ -0,0 +1,15 @@
+
+
+import Clock from './Clock'
+import Counter from './Counter'
+
+const Examples = () => {
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <Clock />
+      <Counter />
+    </div>
+  )
+}
+
+export default Examples
